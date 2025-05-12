bb2e916fd8745ae8aeda1254426dbcd676be253b
DariusMatin <matin@urdu.de.com>
2025-05-12 14:57:02 +0200

    Added merged edge features with GAT

--- DeepLSD/notebooks/lightning_tools/train_gnn_merged.sh
+++ DeepLSD/notebooks/lightning_tools/train_gnn_merged.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=30:00:00 # total time allocated
+#SBATCH --time=24:00:00 # total time allocated
 #SBATCH --mem-per-cpu=16000 # memory per CPU core in MB
 #SBATCH --output=gnn_job.out # specify a file to direct standard output stream,
-#SBATCH --account=3dv # mandatory course tag
+#SBATCH --account=dslab_jobs # mandatory course tag
 #SBATCH --error=gnn_job.err # specify a file to direct standard error stream
 #SBATCH --open-mode=truncate # truncate overwrites output and error files, append just appends
 #SBATCH --mail-type=START,END,FAIL
@@ -14,4 +14,4 @@ conda activate deeplsd
 cd "${HOME}/3D-Vision/DeepLSD/notebooks/lightning_tools" # path to your project folder
 # your command
 export PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True'
-srun python train.py --config=config_matin.yaml
+srun python train_merged.py --config=config_merged.yaml
