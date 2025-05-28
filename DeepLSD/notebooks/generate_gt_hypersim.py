856c71318c63678eaa20479381c5d84e47533f80
Marvin Koch <marvin@marvins-macbook-pro-2.home>
2025-05-28 12:10:58 +0200

    added scannet loader

--- DeepLSD/notebooks/generate_gt_hypersim.py
+++ DeepLSD/notebooks/generate_gt_hypersim.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     
     print("Generate Images")
     
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
     conf = {'detect_lines': True, 'line_detection_params': {'merge': False, 'filtering': True, 'grad_thresh': 3}}
     ckpt = torch.load('../weights/deeplsd_md.tar', map_location=device, weights_only=False)
     net = DeepLSD(conf)
@@ -59,18 +59,22 @@ if __name__ == "__main__":
         for frame_str in tqdm(frames, desc="Processing Frames", leave=False):
             print(f"generate for {image_id}, {frame_str}")
             
-            image_dir = frame_str
+            image_dir = os.path.join(base_dir, image_id)
             required_file = frame_str
+            
             if dataset == "hypersim":
-                image_dir = os.path.join(base_dir, image_id)
                 cam_view_color = "scene_cam_00_final_preview"
 
                 required_file = os.path.join(image_dir, image_id, "images", cam_view_color, f"frame.{frame_str}.color.jpg")
 
-                if not os.path.isfile(required_file):
-                    print(f"Skipping: {os.path.join(required_file, )} does not exist.")
-                    continue
+            elif dataset=="scannet":
+                required_file = os.path.join(image_dir,"color", f"{frame_str}.jpg")
             
+            
+            
+            if not os.path.isfile(required_file):
+                print(f"Skipping: {os.path.join(required_file, )} does not exist.")
+                continue
             process_image(
                 base_dir=base_dir, image_id=image_id, frame_str=frame_str, net=net, device=device, thickness = 1,
                 thresh_normal=8.2e13, thresh_depth=0.2, dataset=dataset, plot=True, file_path = required_file,
